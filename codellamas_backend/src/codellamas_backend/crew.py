7dddd8b4777627d81ef5c8cde36031fdc46acfc7
Michael S <mashtamich1602@gmail.com>
2026-01-22 01:15:54 +0800

    Update crew.py

--- codellamas_backend/src/codellamas_backend/crew.py
+++ codellamas_backend/src/codellamas_backend/crew.py
@@ -2,6 +2,7 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
@@ -26,6 +27,7 @@ class CodellamasBackend():
     def general_agent(self) -> Agent:
         return Agent(
             config=self.agents_config['general_agent'], # type: ignore[index]
+            llm="ollama/phi4",
             verbose=True,
             max_execution_time=300
         )
