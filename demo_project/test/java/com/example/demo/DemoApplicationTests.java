e1edc2236503359592475ffdafeaef7270aa74c8
Michael S <mashtamich1602@gmail.com>
2026-01-21 22:51:17 +0800

    Create demo maven springboot project

---
+++ demo_project/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
