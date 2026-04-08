e52d0d17ccdbefffc195e8606bad5267d415de14
melly19 <melaniepoon.2022@scis.smu.edu.sg>
2026-04-08 12:04:08 +0800

    refactor multi agent

--- backend/src/codellamas_backend/crews/crew_multi.py
+++ backend/src/codellamas_backend/crews/crew_multi.py
@@ -13,8 +13,10 @@ from codellamas_backend.schemas.files import ProjectFile
 
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 BASE_URL = "https://openrouter.ai/api/v1"
-MODEL = "openrouter/qwen/qwen3-coder-30b-a3b-instruct"
+MODEL = "gemini/gemini-3.1-flash-lite-preview"
+# MODEL = "openrouter/qwen/qwen3-coder-30b-a3b-instruct"
 # MODEL = "openrouter/nvidia/nemotron-3-super-120b-a12b:free"
 
 
@@ -113,7 +115,7 @@ class CodellamasBackendMulti:
     def __init__(self, model_name: str = None, api_endpoint: str = None, api_key: str = None):
         self.model_name = model_name or MODEL
         self.api_endpoint = api_endpoint or BASE_URL
-        self.api_key = api_key or OPENROUTER_API_KEY
+        self.api_key = api_key or GEMINI_API_KEY
         self.llm = LLM(
             model=self.model_name,
             base_url=self.api_endpoint,
